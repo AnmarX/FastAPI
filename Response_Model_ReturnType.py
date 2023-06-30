65d2303a9f4379c1530a0125f11e28e8bdc17916
AnmarNabeel <anmarbably1@gmail.com>
2023-06-30 19:12:21 +0300

    .

--- Response_Model_ReturnType.py
+++ Response_Model_ReturnType.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
+from typing import Any
 
 app = FastAPI()
 
@@ -13,10 +14,34 @@ class BaseUser(BaseModel):
 class UserIn(BaseUser):
     password: str
 
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: list[str] = []
 
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item) -> Any:
+    return item
+
+
+@app.get("/items/", response_model=list[Item])
+async def read_items() -> Any:
+    return [
+        {"name": "Portal Gun", "price": 42.0},
+        {"name": "Plumbus", "price": 32.0},
+    ]
+
+
+
+
+# #the arrow indicate what will be returned 
 @app.post("/user/")
 async def create_user(user: UserIn) -> BaseUser:
     return user
+
 
 
 # # the second method 
